60502a2ec5516be9ac34092482a21be39268b15f
Sven Berger <sven@berger-lembeck.de>
2018-10-26 13:42:08 +0200

    ZINFO: entrypoint.sh angepasst

--- unbound/amd64/scripte/entrypoint.sh
+++ unbound/amd64/scripte/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+/usr/sbin/unbound-checkconf
+
+/usr/sbin/unbound-control-setup -d /etc/unbound/
+
 # This won't work if --dns=127.0.0.1 (bootstrapping problems)
 # /etc/periodic/monthly/update-unbound-root-hints
 
@@ -8,9 +12,5 @@ set -e
 /usr/sbin/unbound-anchor -v 4 \
     -a /var/lib/unbound/root.key \
     -r /etc/unbound/root.hint
-
-/usr/sbin/unbound-checkconf
-
-/usr/sbin/unbound-control-setup -d /etc/unbound/
 
 exec "$@"
