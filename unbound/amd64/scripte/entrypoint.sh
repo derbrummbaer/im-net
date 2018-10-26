ef697ef60ffffd1368985eff819293450a1d7122
Sven Berger <sven@berger-lembeck.de>
2018-10-26 09:42:20 +0200

    Entrypoint angepasst

--- unbound/amd64/scripte/entrypoint.sh
+++ unbound/amd64/scripte/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 set -e
 
-/usr/sbin/unbound-checkconf
-
-/usr/sbin/unbound-control-setup -d /etc/unbound/
-
 # This won't work if --dns=127.0.0.1 (bootstrapping problems)
 # /etc/periodic/monthly/update-unbound-root-hints
 
 # From CentOS but not sure how to do on alpine..
-/usr/sbin/unbound-anchor -v4 \
+/usr/sbin/unbound-anchor -v 4 \
     -a /var/lib/unbound/root.key \
-    -r /etc/unbound/root.hints
+    -r /etc/unbound/root.hint
+
+/usr/sbin/unbound-checkconf
+
+/usr/sbin/unbound-control-setup -d /etc/unbound/
 
 exec "$@"
